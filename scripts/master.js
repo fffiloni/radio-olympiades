0fdd03dd8461d3034b8c4bde3993aebbebef6560
Sylvain Filoni <sylvain.fillion15@gmail.com>
2024-03-06 17:34:10 +0100

    Update master.js

--- scripts/master.js
+++ scripts/master.js
@@ -269,7 +269,7 @@ function pickColor() {
 function movePlayHead() {
     playHeadPosition = Date.now();
     if (isIOS == true) {
-        timeElapsed = (playHeadPosition) - (Date.parse(track_start_time));
+        timeElapsed = (playHeadPosition - 6000) - (Date.parse(track_start_time));
     } else {
         timeElapsed = (playHeadPosition + retard) - (Date.parse(track_start_time));
     }
